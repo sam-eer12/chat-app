70f379da56ee552104fcbd4f24f27c7bc054aa43
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-10-02 17:46:38 +0530

    ii

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -28,12 +28,20 @@ const SideBar = ({selectedUser, setSelectedUser}) => {
         </div>
         <div className='flex flex-col'>
             {userDummyData.map((user , index)=>(
-                <div key={index} className={`flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-[#282142] ${selectedUser === user ? "bg-[#282142]" : ""}`} onClick={() => setSelectedUser(user)}>
-                    <img src={user.profilePic} alt={user.fullName} className='w-10 h-10 rounded-full' />
-                    <div className='flex flex-col'>
-                        <span className='text-sm font-medium'>{user.fullName}</span>
-                        <span className='text-xs text-gray-400'>{user.bio}</span>
+                <div onClick={() => setSelectedUser(user)} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id == user._id && 'bg-[#282142]/50'} `}>
+                    <img src={user?.profilePic || assets.avatar_icon} alt={user.fullName} className='w-[35px] aspect-[1/1] rounded-full' />
+                    <div className='flex flex-col leading-5'>
+                        <p>{user.fullName}</p>
+                        {
+                            index < 3
+                            ? <span className='text-xs text-green-400'>Online</span>
+                            : <span className='text-xs text-neutral-400'>Offline</span>
+                        }
                     </div>
+                    {
+                        index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>
+                            {index}</p>
+                    }
                 </div>
             ))}
 
