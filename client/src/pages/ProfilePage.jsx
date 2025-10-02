c85a8d8674bb0016c2e0eb128ebacd58c5bcf97a
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-10-02 14:59:41 +0530

    gg

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Profile Page</h1>
+    </div>
+  )
+}
+
+export default ProfilePage
