fd2d0cf6a904a7e57043f3887cab1234b73e8eef
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-10-02 15:42:00 +0530

    hmm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
