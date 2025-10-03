4621c0556008a161441ae2fe514dd9b4546ecbc5
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-10-03 11:53:54 +0530

    kk

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState("Sign Up")
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Login Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
+      <form  className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>{currState}
+          <img src={assets.arrow_icon} alt=""  className='w-5 cursor-pointer'/>
+        </h2></form>
     </div>
   )
 }
